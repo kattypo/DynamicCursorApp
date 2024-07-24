d952d6c9b660e49abc8905775eedea52a89dda72
kattypo <74074509+kattypo@users.noreply.github.com>
2024-07-24 19:42:53 -0400

    Finished initial HTML / CSS
    Fixed file names, added correct color codes, fixed initial layout, and added solution file

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let sites = ["https://my.unf.edu/",
+    "https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwi4wcHt2cCHAxU5o1oFHVUUAk8YABAAGgJ2dQ&ase=2&gclid=CjwKCAjwzIK1BhAuEiwAHQmU3rmBNj9kdLpf1TeMA8VEWWXDE3UdOb72jXyVjMvm_BWUyk2jAIGlAhoCSxMQAvD_BwE&ohost=www.google.com&cid=CAESVuD2fe8ZftLvzBoPsRND99spgxb228ZuvIoDXelEXrXwuNpcUeiaMofra2hgswG3scgXEU3guFJIsJUVfLhQmPmykAdH9GTNAFSHFQ6aN6833HyPa7Ml&sig=AOD64_0duvMe418_crsRd617lBs6wioGHg&q&nis=4&adurl&ved=2ahUKEwiC37zt2cCHAxXfTDABHcKxB58Q0Qx6BAgGEAE"];
+let list = document.getElementById("UnList");
+for (i = 0; i < data.length; ++i) {
+    var li = document.createElement('li');
+    li.innerText = data[i];
+    list.appendChild(li);
