b34b47d85d021484e1222d8ce26b2b821abd42df
kattypo <74074509+kattypo@users.noreply.github.com>
2024-07-26 20:38:11 -0400

    Added code to display stored sites
    Added code to dynamically create a unordered list from stored sites and display the list

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 'use strict';
 
 let sites = ["https://my.unf.edu/",
-    "https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwi4wcHt2cCHAxU5o1oFHVUUAk8YABAAGgJ2dQ&ase=2&gclid=CjwKCAjwzIK1BhAuEiwAHQmU3rmBNj9kdLpf1TeMA8VEWWXDE3UdOb72jXyVjMvm_BWUyk2jAIGlAhoCSxMQAvD_BwE&ohost=www.google.com&cid=CAESVuD2fe8ZftLvzBoPsRND99spgxb228ZuvIoDXelEXrXwuNpcUeiaMofra2hgswG3scgXEU3guFJIsJUVfLhQmPmykAdH9GTNAFSHFQ6aN6833HyPa7Ml&sig=AOD64_0duvMe418_crsRd617lBs6wioGHg&q&nis=4&adurl&ved=2ahUKEwiC37zt2cCHAxXfTDABHcKxB58Q0Qx6BAgGEAE"];
-let list = document.getElementById("UnList");
-for (i = 0; i < data.length; ++i) {
-    var li = document.createElement('li');
-    li.innerText = data[i];
-    list.appendChild(li);
+    "https://mail.google.com/", "https://getbootstrap.com/", "https://stackoverflow.com/", "https://chromewebstore.google.com/?hl=en",
+    "https://www.uwgb.edu/"];
+
+function getUserInformation() {
+    getSiteList();
+}
+function getSiteList() {
+    let list = document.getElementById("siteList");
+    for (let i = 0; i < sites.length; ++i) {
+        let li = document.createElement('li');
+        li.innerHTML = sites[i];
+        list.appendChild(li);
+    }
+}
