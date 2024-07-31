67198c0bdbb61a53ec0f9b6420ed3bc9a8ffcc71
kattypo <74074509+kattypo@users.noreply.github.com>
2024-07-31 00:24:54 -0400

    Added to main page
    Added functions. Main connects to the Add Site page through the "Add Site" button

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 'use strict';
 
+let defaultCursorStroke = "black";
+let defaultCursorFill = "white";
 let sites = ["https://my.unf.edu/",
     "https://mail.google.com/", "https://getbootstrap.com/", "https://stackoverflow.com/", "https://chromewebstore.google.com/?hl=en",
-    "https://www.uwgb.edu/"];
+    "https://www.uwgb.edu/", "pbs.com"];
 
-function getUserInformation() {
+
+window.addEventListener('load', function () {
+    let addSiteBtn = document.getElementById("addSiteBtn");
+    addSiteBtn.addEventListener("click", function () {
+        loadSitePage();
+    });
+});
+function loadMainPage() {
     getSiteList();
+    getDefaultCursor();
+}
+function loadSitePage() {
+    window.location.href = "addSite.html";
 }
 function getSiteList() {
     let list = document.getElementById("siteList");
@@ -14,4 +27,9 @@ function getSiteList() {
         li.innerHTML = sites[i];
         list.appendChild(li);
     }
+}
+function getDefaultCursor() {
+
+    document.getElementById('cursorPath').style.stroke = defaultCursorStroke;
+    document.getElementById('cursorPath').style.fill = defaultCursorFill;
 }
