19d4dcbab0f11238b165492f04ed1a9e976233f0
kattypo <74074509+kattypo@users.noreply.github.com>
2024-08-23 16:13:04 -0400

    Cleaned up code
    Removed unused button and edited variables

--- addSite.js
+++ addSite.js
@@ -4,15 +4,10 @@ let site = { //dictionary to be stored
     fillSetting: "#000000"
 };
 window.addEventListener('load', function () {
-    let backBtn = document.getElementById("backBtn");
-    let applyBtn = document.getElementById("applyBtn");
-    let strokeColorPicker = document.getElementById("strokeColorPicker");
-    let fillColorPicker = document.getElementById("fillColorPicker");
-    //let darkStrokeColorPicker = document.getElementById("darkStrokeColorPicker");
-    //let darkFillColorPicker = document.getElementById("darkFillColorPicker");
-    //let darkModeBtn = document.getElementById("darkModeBtn");
-    let customImageFile = document.getElementById("customImageFile");
-
+    const backBtn = document.getElementById("backBtn");
+    const applyBtn = document.getElementById("applyBtn");
+    const strokeColorPicker = document.getElementById("strokeColorPicker");
+    const fillColorPicker = document.getElementById("fillColorPicker");
     backBtn.addEventListener("click", function () {
         window.location.href = "main.html";
     });
@@ -23,7 +18,7 @@ window.addEventListener('load', function () {
         }
     });
     strokeColorPicker.addEventListener("input", function (event) {
-        let customCursorPath = document.getElementById("customCursorPath");
+        const customCursorPath = document.getElementById("customCursorPath");
         customCursorPath.style.stroke = event.target.value;
     });
     strokeColorPicker.addEventListener("change", function (event) {
@@ -36,44 +31,14 @@ window.addEventListener('load', function () {
     fillColorPicker.addEventListener("change", function (event) {
         site.fillSetting = event.target.value;
     });
-    //revamp to be used for the hand icon
-    //darkStrokeColorPicker.addEventListener("input", function (event) {
-    //    let darkCursorPath = document.getElementById("darkCursorPath");
-    //    darkCursorPath.style.stroke = event.target.value;
-    //});
-    //darkStrokeColorPicker.addEventListener("change", function (event) {
-    //    darkStrokeColor = event.target.value;
-    //});
-    //darkFillColorPicker.addEventListener("input", function (event) {
-    //    let darkCursorPath = document.getElementById("darkCursorPath");
-    //    darkCursorPath.style.fill = event.target.value;
-    //});
-    //darkFillColorPicker.addEventListener("change", function (event) {
-    //    darkFillColor = event.target.value;
-    //});
-    //darkModeBtn.addEventListener("click", function () {
-    //    let darkModeSettings = document.getElementById("darkModeSettings");
-    //    if (darkModeSettings.style.display !== 'flex') {
-    //        darkModeSettings.style.display = 'flex';
-    //        darkModeBtn.innerHTML = "Disable Dark Mode";
-    //        darkModeEnabled = true;
-    //    }
-    //    else {
-    //        darkModeSettings.style.display = 'none';
-    //        darkModeBtn.innerHTML = "Enable Dark Mode";
-    //        darkModeEnabled = false;
-    //    }
-    //});
     loadSitePage();
 });
 function loadSitePage() {
     let domain;
-    let siteNameLabel = document.getElementById("siteNameLabel");
+    const siteNameLabel = document.getElementById("siteNameLabel");
     (async () => {
         const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-        let name = tab.url;
-        let parsedUrl = new URL(name);
-        domain = parsedUrl.hostname;
+        domain = new URL(tab.url).hostname;
         siteNameLabel.innerHTML = domain;
         site.name = domain;
     })();
@@ -84,9 +49,8 @@ function addToStorage() {
             console.error('Error retrieving stored settings:', chrome.runtime.lastError);
             return;
         }
-        let siteSettings = result.siteSettings || [];
+        const siteSettings = result.siteSettings || [];
         siteSettings.push(site);
-        console.log('New array:', siteSettings);
         chrome.storage.sync.set({ siteSettings: siteSettings }, function () {
             if (chrome.runtime.lastError) {
                 console.error('Error updating stored settings:', chrome.runtime.lastError);
@@ -96,4 +60,3 @@ function addToStorage() {
         });
     });
 }
-//add function to check if its already been added to the list and return a error
