97c6a16b20a4b0eea7039aba58cc3342411c6bef
kattypo <74074509+kattypo@users.noreply.github.com>
2024-08-25 15:24:24 -0400

    Added send feedback page

---
+++ sendFeedback.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function () {
+    const backBtn = document.getElementById("backBtn");
+    backBtn.addEventListener("click", function () {
+        window.location.href = "main.html";
+    });
+});
